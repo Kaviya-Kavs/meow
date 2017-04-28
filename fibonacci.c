bddc817c128fa078d729d5aadb85af299a9a6622
Kaviya-Kavs <mkaviyamohan96@gmail.com>
2017-04-28 09:14:04 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,18 @@
+# meow
+int main()
+{
+int i,range;
+int long double a[40];
+printf("enter the range");
+scanf("%d",&range);
+a[0]=0;
+a[1]=1;
+for(i=2;i<range;i++)
+{
+a[i]=a[i-1]+a[i-2]
+}
+printf("fibonacci series is");
+for(i=0;i<range;i++)
+printf("%1d",&range);
+return 0;
+}
