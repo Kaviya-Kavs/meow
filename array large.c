fe44f923b4c54c09163e123ca78d765d85af99c6
Kaviya-Kavs <mkaviyamohan96@gmail.com>
2017-06-09 10:33:39 +0530

    Create array large.c

---
+++ array large.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[5],i,j,temp;
+clrscr();
+for(i=0;i<5;i++){
+scanf("%d",&a[i])
+}
+for(i=0;i<5;i++){
+for(j=0;j<n;j++){
+if a[i]>a[j]{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}}
+for(i=0;i<5;i++){
+printf("%d",&a[i])}
+getch();
+}
